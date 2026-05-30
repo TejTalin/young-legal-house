b28421a69bf6ed2dfd4951f1576ac1de20e71fd9
TejTalin <131414974+TejTalin@users.noreply.github.com>
2026-05-30 14:36:25 +0530

    Add files via upload

--- components/NetworkBackground.jsx
+++ components/NetworkBackground.jsx
@@ -1,14 +1,73 @@
+const nodes = [
+  { x: 7, y: 17, icon: 1, size: 44 },
+  { x: 20, y: 29, icon: 6, size: 38 },
+  { x: 36, y: 18, icon: 8, size: 42 },
+  { x: 52, y: 32, icon: 4, size: 46 },
+  { x: 68, y: 18, icon: 3, size: 42 },
+  { x: 86, y: 30, icon: 2, size: 44 },
+  { x: 94, y: 15, icon: 5, size: 38 },
+  { x: 12, y: 56, icon: 5, size: 42 },
+  { x: 28, y: 68, icon: 7, size: 44 },
+  { x: 44, y: 54, icon: 1, size: 46 },
+  { x: 61, y: 70, icon: 6, size: 40 },
+  { x: 78, y: 55, icon: 8, size: 45 },
+  { x: 92, y: 73, icon: 4, size: 44 },
+  { x: 5, y: 84, icon: 3, size: 38 },
+  { x: 35, y: 90, icon: 2, size: 42 },
+  { x: 70, y: 88, icon: 7, size: 40 },
+];
+
+const connections = [
+  [0, 1],
+  [1, 2],
+  [2, 3],
+  [3, 4],
+  [4, 5],
+  [5, 6],
+  [1, 7],
+  [3, 9],
+  [5, 11],
+  [7, 8],
+  [8, 9],
+  [9, 10],
+  [10, 11],
+  [11, 12],
+  [7, 13],
+  [8, 14],
+  [10, 15],
+];
+
 export default function NetworkBackground() {
   return (
     <div className="community-bg" aria-hidden="true">
-      <div className="legal-atmosphere legal-atmosphere-left" />
-      <div className="legal-atmosphere legal-atmosphere-right" />
-      <div className="legal-grid" />
-      <svg className="legal-lines" viewBox="0 0 1440 900" preserveAspectRatio="none">
-        <path d="M90 210 C280 120, 410 260, 610 180 S930 120, 1160 230 S1370 240, 1460 150" />
-        <path d="M-40 620 C180 520, 360 720, 560 580 S900 500, 1090 650 S1300 780, 1490 590" />
-        <path d="M220 840 C380 650, 520 760, 700 610 S980 420, 1240 520" />
+      <svg className="network-lattice" viewBox="0 0 100 100" preserveAspectRatio="none">
+        {connections.map(([from, to], index) => (
+          <line
+            key={`${from}-${to}-${index}`}
+            x1={nodes[from].x}
+            y1={nodes[from].y}
+            x2={nodes[to].x}
+            y2={nodes[to].y}
+          />
+        ))}
       </svg>
+
+      <div className="network-icons">
+        {nodes.map((node, index) => (
+          <img
+            key={`${node.icon}-${index}`}
+            src={`/network-icons/legal-icon-${node.icon}.png`}
+            alt=""
+            className="network-icon"
+            style={{
+              left: `${node.x}%`,
+              top: `${node.y}%`,
+              width: `${node.size}px`,
+              height: `${node.size}px`,
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
