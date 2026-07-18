573af9ecc111340518f29f445415d88bbb062a23
TejTalin <131414974+TejTalin@users.noreply.github.com>
2026-07-18 21:42:48 +0530

    Delete events directory

--- events/thank-you/page.jsx
+++
@@ -1,6 +0,0 @@
-import ThankYouClient from './ThankYouClient';
-
-export default function EventThankYouPage({ searchParams }) {
-  const participantName = searchParams?.name || 'Participant';
-  return <ThankYouClient participantName={participantName} />;
-}
